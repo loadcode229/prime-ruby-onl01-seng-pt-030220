0a0d2d81c07c55c1c2546f734e06b1548315c6a0
Adam Priest <sepone229@gmail.com>
2020-03-12 22:29:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,4 @@
 def prime?(num)
-  i = 2
-  while i < num
-    return false if num % i == 0
-    i += 1
-  end
-  return true
-end
-
-def prime?(num)
   i = 1
   if num <= 1 || num == 0 || num == 1
     return false
