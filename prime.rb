a137fd3a9dc8af0a9a367bf6e6b49d95a461e80a
Adam Priest <sepone229@gmail.com>
2020-03-12 22:29:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,9 +11,11 @@ def prime?(num)
   i = 1
   if num <= 1 || num == 0 || num == 1
     return false
-  elsif 2(i-1).each do |i|
-    if num % i == 0
-      return false
+  elsif 
+    2(i-1).each do |i|
+      if num % i == 0
+        return false
+      end
     end
   end
   true
