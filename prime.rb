ca1bc2b16a83029893eee5809171db00ec38666e
Adam Priest <sepone229@gmail.com>
2020-03-12 22:18:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,4 @@
 def prime?(num)
-  i = 2
-  while i < num
-    return false if num % i == 0
-    i += 1
-  end
-  true
-end
-def prime?(num)
   (2..(num - 1)).each do |i|
     return false if num % i == 0
   end
